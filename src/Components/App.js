7e06ad701566376748b9eff4d9cbed2271d95bd1
Vladimir Kravchenko <kvv1793@gmail.com>
2021-06-09 18:19:11 +0300

    done

--- src/Components/App.js
+++ src/Components/App.js
@@ -6,11 +6,19 @@ import Statistics from "./statistics/Statistics";
 import TransactionHistory from "./transactionHistory/TransactionHistory";
 import styles from "./App.module.css";
 
+const { user } = data;
+
 const App = () => {
   console.log(data);
   return (
     <div className={styles.container}>
-      <Profile />
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
       <Statistics statisticalData={data.statisticalData} title="Upload stats" />
       <FriendList friend={data.friends} />
       <TransactionHistory transactions={data.transactions} />
--- src/Components/profile/Profile.js
+++ src/Components/profile/Profile.js
@@ -1,29 +1,29 @@
 import React from "react";
-import user from "../../data/user.json";
+// import user from "../../data/user.json";
 import styles from "./Profile.module.css";
 
-const Profile = () => {
+const Profile = ({ name, tag, location, avatar, stats }) => {
   return (
     <div className={styles.profile}>
       <div className={styles.description}>
-        <img src={user.avatar} alt={user.name} className={styles.avatar} />
-        <p className={styles.name}>{user.name}</p>
-        <p className={styles.tag}>{user.tag}</p>
-        <p className={styles.location}>{user.location}</p>
+        <img src={avatar} alt={name} className={styles.avatar} />
+        <p className={styles.name}>{name}</p>
+        <p className={styles.tag}>{tag}</p>
+        <p className={styles.location}>{location}</p>
       </div>
 
       <ul className={styles.stats}>
         <li className={styles.listItem}>
           <span className="label">Followers</span>
-          <span className="quantity">{user.stats.followers}</span>
+          <span className="quantity">{stats.followers}</span>
         </li>
         <li className={styles.listItem}>
           <span className="label">Views</span>
-          <span className="quantity">{user.stats.views}</span>
+          <span className="quantity">{stats.views}</span>
         </li>
         <li className={styles.listItem}>
           <span className="label">Likes</span>
-          <span className="quantity">{user.stats.likes}</span>
+          <span className="quantity">{stats.likes}</span>
         </li>
       </ul>
     </div>
